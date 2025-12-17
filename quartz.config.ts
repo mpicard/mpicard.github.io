e52b466aab84978d49bc7656ae26e6bc51c4249c
mpicard <mpicard@users.noreply.github.com>
2025-12-17 15:18:52 +0100

    build: github pages

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "mpicard.github.io/obsidian-quartz",
     ignorePatterns: [
       "private",
       "Templates",
